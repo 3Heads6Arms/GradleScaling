308402e813b278f89eaefbe0a1e3814a8a6514ef
anh.hoang <h.anh.viet91@gmail.com>
2017-10-15 01:19:31 +0300

    added test to AsyncTask

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAsyncTaskTest.java
@@ -0,0 +1,36 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by anh.hoang on 10/15/17.
+ */
+@RunWith(AndroidJUnit4.class)
+public class JokeAsyncTaskTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldReturnNonEmptyString() {
+        MainActivityFragment.JokeAsyncTask asyncTask = new MainActivityFragment.JokeAsyncTask(activityTestRule.getActivity(), null);
+
+        String result = null;
+        try {
+            result = asyncTask.execute().get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result.length() > 0);
+    }
+}
